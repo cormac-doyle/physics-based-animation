0aaeccc8659fbed279aebae53dc09225bf28c0da
Cormac Doyle <doylec54@tcd.ie>
2022-01-30 16:15:43 +0000

    fire ball with goal posts

--- Assets/Scripts/CopyMotion.cs
+++ Assets/Scripts/CopyMotion.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,25 +8,26 @@ public class CopyMotion : MonoBehaviour
 
     public Transform targetLimb;
     ConfigurableJoint configurableJoint;
-    public bool mirror;
+
+    Quaternion initalJointRotation;
+
     // Start is called before the first frame update
     void Start()
     {
         configurableJoint = GetComponent<ConfigurableJoint>();
+
+        initalJointRotation = configurableJoint.transform.localRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!mirror)
-        {
-            configurableJoint.targetRotation = targetLimb.rotation;
+        
+    }
 
-        }
-        else
-        {
-            configurableJoint.targetRotation = Quaternion.Inverse(targetLimb.rotation);
+    private void FixedUpdate()
+    {
+        ConfigurableJointExtensions.SetTargetRotationLocal(configurableJoint, targetLimb.localRotation, initalJointRotation);
 
-        }
     }
 }
--- Assets/Scripts/GoalKeeper.cs
+++ Assets/Scripts/GoalKeeper.cs
@@ -2,26 +2,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoalKeeper : MonoBehaviour
 {
 
     public Animator anim;
     public LayerMask layermask;
+    public GameObject soccerBall;
+    float shootingAngle;
+
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        shootingAngle = 2.9f;
+        Invoke("ShootBall",2f);
+        Invoke("PlayAnimation", 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    private void ShootBall()
+    {
+        soccerBall.velocity = new Vector3(shootingAngle, 5, -6);
+    }
+
+    private void PlayAnimation()
+    {
+
+        
         LongDiveRight();
         LongDiveLeft();
         ShortDiveRight();
         ShortDiveLeft();
-
     }
 
     private void FixedUpdate()
