45c25e213869cbe0e7f68ecaf65a0bd1198bc2a3
Cormac Doyle <doylec54@tcd.ie>
2022-01-30 12:17:28 +0000

    goalkeeper animations

--- Assets/Scripts/GoalKeeper.cs
+++ Assets/Scripts/GoalKeeper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,20 +11,62 @@ public class GoalKeeper : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         LongDiveRight();
+        LongDiveLeft();
+        ShortDiveRight();
+        ShortDiveLeft();
+
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void ShortDiveLeft()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            this.anim.SetTrigger("shortDiveLeft");
+            
+        }
+    }
+
+    private void ShortDiveRight()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            this.anim.SetTrigger("shortDiveRight");
+            
+
+        }
+    }
+
+    private void LongDiveLeft()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            this.anim.SetTrigger("longDiveLeft");
+            
+
+        }
     }
 
     private void LongDiveRight()
     {
         if (Input.GetKeyDown(KeyCode.K))
         {
-            this.anim.SetBool("dive", true);
+            this.anim.SetTrigger("longDiveRight");
+            
+
         }
     }
+
+    
 }
