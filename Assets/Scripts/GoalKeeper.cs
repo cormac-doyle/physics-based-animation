814227190e4db3e956a4863a7edb3b00bf28190d
Cormac Doyle <doylec54@tcd.ie>
2022-01-31 21:27:47 +0000

    fire ball at goal

--- Assets/Scripts/GoalKeeper.cs
+++ Assets/Scripts/GoalKeeper.cs
@@ -9,17 +9,15 @@ public class GoalKeeper : MonoBehaviour
 
     public Animator anim;
     public GameObject soccerBall;
-    
-    float shootingAngleX;
 
+    Camera cam;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
 
-        shootingAngleX = UnityEngine.Random.Range(-2.8f, 2.8f);
-        Invoke("ShootBall",5f);
-        Invoke("PlayAnimation", 5f);
+        cam = GameObject.Find("Camera").GetComponent<Camera>();
+
     }
 
     // Update is called once per frame
@@ -29,25 +27,62 @@ public class GoalKeeper : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R))
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ShootBall(Input.mousePosition.x,Input.mousePosition.y);
+            
+        }
+
         LongDiveLeft();
         LongDiveRight();
         ShortDiveLeft();
         ShortDiveRight();
     }
 
-    private void ShootBall()
+    private void ShootBall(float mousePosX, float mousePosY)
     {
-        soccerBall.GetComponent<Rigidbody>().velocity = new Vector3(shootingAngleX, 5, -6);
+        Vector2 shootVelocities = calcShootVelocities(mousePosX, mousePosY);
+
+        PlayAnimation(-shootVelocities.x);
+
+        soccerBall.GetComponent<Rigidbody>().velocity = new Vector3(-shootVelocities.x, shootVelocities.y, -6.5f);
     }
 
-    private void PlayAnimation()
+    private Vector2 calcShootVelocities(float mousePosX, float mousePosY)
+    {
+        float MOUSE_ERROR_X = -15f;
+        float MOUSE_ERROR_Y = 15f;
+
+        mousePosX -= MOUSE_ERROR_X;
+        mousePosY -= MOUSE_ERROR_Y;
+
+        float postRightX = cam.WorldToScreenPoint(GameObject.FindGameObjectWithTag("PostRight").transform.position).x;
+        float postLeftX = cam.WorldToScreenPoint(GameObject.FindGameObjectWithTag("PostLeft").transform.position).x;
+
+        float shootingAngleX = mapToRange(mousePosX, postRightX, postLeftX, -2.8f, 2.8f);
+
+
+        float crossBarPosY = cam.WorldToScreenPoint(GameObject.FindGameObjectWithTag("CrossBar").transform.position).y;
+        float groundPosY = cam.WorldToScreenPoint(GameObject.FindGameObjectWithTag("Ground").transform.position).y;
+
+        float shootingAngleY = mapToRange(mousePosY, groundPosY, crossBarPosY, 5.3f, 7.35f);
+
+        return new Vector2(shootingAngleX, shootingAngleY);
+    }
+
+    private float mapToRange(float input, float input_start,float input_end, float output_start, float output_end)
+    {
+        return (input - input_start) / (input_end - input_start) * (output_end - output_start) + output_start;
+    }
+    
+    private void PlayAnimation(float shootingAngleX)
     {
 
-        this.anim.SetTrigger(calculateAppropiateAnimation());
+        this.anim.SetTrigger(calculateAppropiateAnimation(shootingAngleX));
 
     }
 
-    private String calculateAppropiateAnimation()
+    private String calculateAppropiateAnimation(float shootingAngleX)
     {
         if (shootingAngleX > 1.5f)
         {
